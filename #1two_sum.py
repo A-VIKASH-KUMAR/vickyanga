3da99b537ca70d8a49c80f6c94c7454550e19d5e
A-VIKASH-KUMAR <vickyanga@gmail.com>
2024-01-05 19:35:28 +0530

    add two sum python functiom whih pass alll test cases

---
+++ #1two_sum.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def twoSum(nums, target):
+        num_dict = {}
+        n = len(nums) 
+        # create hash table map
+        for i, num in enumerate(nums):
+            num_dict[num] = i
+        
+        print("num dict", num_dict)
+        for i in range(n):
+              complement = target - nums[i]
+              print("complement out",complement)
+              if complement in num_dict and num_dict[complement] != i:
+                    print(complement)
+                    return {i, num_dict[complement]}
+
+print(twoSum(nums=[3,2,4],target=6))
